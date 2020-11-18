fc548cdb79f7cb1a3d5846bab08e3afc5838a46d
Alex P <Uzit0070@gmail.com>
2020-11-18 21:44:58 +0500

    create store.js, change data in v-catalog

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
         <h1 class="catalog__title">Catalog</h1>
         <div class="catalog__inner">
             <v-catalog-item 
-                v-for="product in products"
+                v-for="product in PRODUCTS"
                 :key="product.article"
                 :product__data="product"
                 @sendArticle="consoleLogItems" 
@@ -14,72 +14,34 @@
 
 <script>
 import vCatalogItem from './v-catalog-item'
+import {mapActions, mapGetters} from 'vuex'
 
 export default {
     name: 'v-catalog',
     props: {},
     data() {
         return {
-            "products": [
-                {
-                image: "1.jpg",
-                name: "T-shirt 1",
-                price: 2100.234234234,
-                article: "T1",
-                available: true,
-                category: "Мужские"
-                },
-                {
-                image: "2.jpg",
-                name: "T-shirt 2",
-                price: 3150.12312412,
-                article: "T2",
-                available: true,
-                category: "Женские"
-                },
-                {
-                image: "1.jpg",
-                name: "T-shirt 3",
-                price: 4200.51524,
-                article: "T3",
-                available: false,
-                category: "Женские"
-                },
-                {
-                image: "4.jpg",
-                name: "T-shirt 4",
-                price: 5300.1245512,
-                article: "T4",
-                available: true,
-                category: "Мужские"
-                },
-                {
-                image: "5.jpg",
-                name: "T-shirt 5",
-                price: 6500.3522314,
-                article: "T5",
-                available: false,
-                category: "Женские"
-                },
-                {
-                image: "6.jpeg",
-                name: "T-shirt 6",
-                price: 8700.4124123,
-                article: "T6",
-                available: true,
-                category: "Женские"
-                }
-            ]
+            
         }
     },
     components: {
         vCatalogItem,
     },
-    computed: {},
+    computed: {
+        ...mapGetters([
+            'PRODUCTS'
+        ]),
+    },
     methods: {
+        ...mapActions([
+            'GET_PRODUCTS_FROM_API'
+        ]),
         consoleLogItems(data) {
             console.log(data);
         }
+    },
+    mounted() {
+        this.GET_PRODUCTS_FROM_API()
     }
 }
 </script>
